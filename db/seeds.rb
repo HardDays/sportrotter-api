3f6cc69ef61b3a51d6072941712c496ff8a29545
shvv733kn@gmail.com <shvv733kn@gmail.com>
2017-09-18 16:04:44 +0300

    passfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-@user = User.new(email: :admin, password: User.get_hash('admin'), user_type: :professional)
+@user = User.new(email: :admin, password: 'admin', user_type: :professional)
 if !@user.save
     puts json: @user.errors
 end
